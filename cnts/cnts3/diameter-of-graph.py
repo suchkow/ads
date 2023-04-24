69201118b1944400b1f4fe01e29882b15cb3718b
suchkow <makeeoutchill@gmail.com>
2023-04-25 00:58:15 +0300

    some changes

--- cnts/cnts3/diameter-of-graph.py
+++ cnts/cnts3/diameter-of-graph.py
@@ -1,20 +1,17 @@
 N = int(input())
 I = float('inf')
 
-matrix = [
-    [map(int, input().split())] for _ in range(N)
-]
+matrix = [[map(int, input().split())] for _ in range(N)]
 dist = [I] * N
 
 dist[0] = 0
 for i in range(N):
     for u in range(N):
         for v in range(N):
-            if matrix[u][v] != -1 and dist[u] != float('inf') and dist[u] + matrix[u][v] < dist[v]:
+            if (matrix[u][v] != -1) and (dist[u] != I) and (dist[u] + matrix[u][v] < dist[v]):
                 dist[v] = dist[u] + matrix[u][v]
 
 d, r = 0, I
-
 for i in range(N):
     for j in range(N):
         if matrix[i][j] != -1:
